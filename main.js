19e47ca708a08efc98d5902f3d8d4c28cfdf09f1
Erick Boch <erickboch67@gmail.com>
2021-08-30 23:52:39 -0500

    1.05 portafolio

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/*
 const grid = new Muuri('.grid',{
     layout:{
         rounding: false
@@ -28,4 +29,54 @@ document.querySelectorAll('.grid .item img').forEach((elemento)=>{
        // overlay.classList.remove()
        evento.target.id === "overlay" ? overlay.classList.remove('activo'):'';
     })
+});*/
+
+$(".menu-toggle-btn").click(function(){
+    $(this).toggleClass("fa-times");
+    $(".navigation-menu").toggleClass("active");
 });
+
+//scrolReveal
+window.sr = ScrollReveal();
+  sr.reveal('.div', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px'
+  });
+  
+  sr.reveal('.div-item', {
+    duration: 3100,
+    origin: 'bottom',
+    distance: '-100px'
+  });
+  sr.reveal('.container-p', {
+    duration: 3100,
+    origin: 'top',
+    distance: '-100px'
+  });
+
+  sr.reveal('.container-item', {
+    duration: 3000,
+    origin: 'left',
+    distance: '-100px'
+  });
+  sr.reveal('.container-item-p', {
+    duration: 3000,
+    origin: 'right',
+    distance: '-100px'
+  });
+  sr.reveal('.form', {
+    duration: 3000,
+    origin: 'left',
+    distance: '-100px'
+  });
+  sr.reveal('.datos-p', {
+    duration: 3000,
+    origin: 'right',
+    distance: '-100px'
+  });
+  sr.reveal('.redes', {
+    duration: 3000,
+    origin: 'top',
+    distance: '-100px'
+  });
